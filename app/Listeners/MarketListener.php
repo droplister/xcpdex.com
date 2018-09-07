828820a9af702fe58b4ab6a55f166904cea38205
Dan Anderson <anderson@familymedia.org>
2018-09-06 22:43:23 -0400

    Update Listener

--- app/Listeners/MarketListener.php
+++ app/Listeners/MarketListener.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Market;
+use Droplister\XcpCore\App\Asset;
 use Droplister\XcpCore\App\Events\OrderWasCreated;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -17,10 +18,19 @@ class MarketListener
      */
     public function handle(OrderWasCreated $event)
     {
+        // Base Asset
+        $base = explode('/', $event->order->trading_pair_normalized)[0];
+        $base_asset = Asset::where('asset_name', '=', $base)->orWhere('asset_longname', '=', $base)->first();
+
+        // Quote Asset
+        $quote = explode('/', $event->order->trading_pair_normalized)[1];
+        $quote_asset = Asset::where('asset_name', '=', $quote)->orWhere('asset_longname', '=', $quote)->first();
+
+        // Trading Pair
         Market::firstOrCreate([
-            'xcp_core_base_asset' => explode('/', $event->order->trading_pair_normalized)[0],
-            'xcp_core_quote_asset' => explode('/', $event->order->trading_pair_normalized)[1],
-            'name' => $event->order->trading_pair_normalized,
+            'xcp_core_base_asset' => $base_asset->asset_name,
+            'xcp_core_quote_asset' => $quote_asset->asset_name,
+            'name' => "{$base_asset->display_name}/{$quote_asset->display_name}",
         ]);
     }
 }

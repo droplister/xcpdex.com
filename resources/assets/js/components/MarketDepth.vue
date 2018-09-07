a90aaa2e2ca300fe5e56f81379d6f3a99c49b93f
Dan Anderson <anderson@familymedia.org>
2018-09-07 16:05:00 -0400

    Update parseFloat

--- resources/assets/js/components/MarketDepth.vue
+++ resources/assets/js/components/MarketDepth.vue
@@ -62,7 +62,7 @@ export default {
       var runningTotal = 0
       var i = 0
       for (i = 0; i < data.length; i++) {
-        runningTotal += parseFloat(data[i][1]).format('0.00000000')
+        runningTotal += parseFloat(data[i][1]).toFixed(8)
         accumulation.push([data[i][0], runningTotal])
       }
       return accumulation

0f188bd198bccb12e503964794a9e1e528c3979f
Dan Anderson <anderson@familymedia.org>
2018-09-13 10:09:28 -0400

    Update Markets Controller

--- app/Http/Controllers/MarketsController.php
+++ app/Http/Controllers/MarketsController.php
@@ -43,14 +43,7 @@ class MarketsController extends Controller
     public function show(Request $request, Market $market)
     {
         // Market's Last Match
-        $last_match = OrderMatch::where('backward_asset', '=', $market->xcp_core_base_asset)
-            ->where('forward_asset', '=', $market->xcp_core_quote_asset)
-            ->where('status', '=', 'completed')
-            ->orWhere('backward_asset', '=', $market->xcp_core_quote_asset)
-            ->where('forward_asset', '=', $market->xcp_core_base_asset)
-            ->where('status', '=', 'completed')
-            ->orderBy('tx1_index', 'desc')
-            ->first();
+        $last_match = $market->lastMatch();
 
         // Features
         $features = Feature::highestBids()->with('market')->get();

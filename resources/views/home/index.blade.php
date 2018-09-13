d9784d1a6ff76ffe92df70c91a580d6d614348c8
Dan Anderson <anderson@familymedia.org>
2018-09-13 11:50:50 -0400

    Update Copy

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,7 +6,7 @@
     <h1 class="mb-3">Counterparty DEX</h1>
     <hr />
     <p>
-        <span class="d-none d-md-inline-block">The Counterparty DEX is a decentralized exchange that allows anyone, anywhere to trade Counterparty assets on the Bitcoin blockchain.</span> XCPDEX.com is a blockchain explorer dedicated to publishing Counterparty DEX data, like trade volumes and prices, but XCPDEX.com itself is NOT an exchange.
+        <span class="d-none d-md-inline-block">The Counterparty DEX is a decentralized exchange that allows anyone, anywhere to trade Counterparty assets on the Bitcoin blockchain.</span> XCPDEX.com is a blockchain explorer dedicated to publishing Counterparty DEX data, but XCPDEX.com is not in and of itself an exchange.
     </p>
     <div class="row justify-content-center mb-2">
         <div class="col-12">

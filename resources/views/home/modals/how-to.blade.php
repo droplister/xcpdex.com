5e54fe5d83b5f54b4db6637f8bbc0b8b51286366
Dan Anderson <anderson@familymedia.org>
2018-09-13 01:22:12 -0400

    Update

--- resources/views/home/modals/how-to.blade.php
+++ resources/views/home/modals/how-to.blade.php
@@ -8,12 +8,11 @@
                 </button>
             </div>
             <div class="modal-body">
-                <p>On XCPDEX.com, anyone can feature any market. The way it works is simple...</p>
-                <p>We run an on-going ad auction:</p>
-                <p>The two (2) highest bids get their market featured on the homepage.</p>
-                <p>The four (4) highest bids appear sitewide in the XCPDEX.com sidebar.</p>
                 <p>We have a special address that we monitor: <br /> <b><a href="https://xchain.io/address/{{ config('xcpdex.feature_address') }}" target="_blank">{{ config('xcpdex.feature_address') }}</a></b></p>
-                <p>Send your bid to this address (XCP ONLY) and include the name of the market you want featured in the memo field. As an example, you would enter either "XCP_BTC" or "XCP/BTC".</p>
+                <p>Anyone can send XCP to this address and enter the name of the market they want featured in the memo field. For example, "PEPECASH/XCP".</p>
+                <p>- The two (2) highest bids get their market featured on the homepage.</p>
+                <p>- The four (4) highest bids appear sitewide in the XCPDEX.com sidebar.</p>
+                <p>- Each market will remain featured until or unless they get outbid.</p>
                 <p>The current high bids are as follows:</p>
                 <p>
                 @foreach($features as $featured)

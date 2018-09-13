c18de25b195100f398afa4931a2e3d30cbe4f8a8
Dan Anderson <anderson@familymedia.org>
2018-09-13 12:12:02 -0400

    Update

--- app/Http/Resources/MarketResource.php
+++ app/Http/Resources/MarketResource.php
@@ -17,7 +17,7 @@ class MarketResource extends Resource
         return [
             'name' => $this->name,
             'slug' => $this->slug,
-            'market_cap' => number_format($this->baseAsset->supply_normalized * $this->lastMatch()->trading_price_normalized),
+            'market_cap' => $this->lastMatch() ? number_format($this->baseAsset->supply_normalized * $this->lastMatch()->trading_price_normalized) : 0,
             'volume' => number_format($this->volume_normalized),
             'supply' => number_format($this->baseAsset->supply_normalized),
             'base_asset' => $this->baseAsset->display_name,

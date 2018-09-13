132ac25720d2dc44689f5a2e5ed24932c43ffc7b
Dan Anderson <anderson@familymedia.org>
2018-09-13 03:38:38 -0400

    Update Mempool

--- resources/assets/js/components/Mempool.vue
+++ resources/assets/js/components/Mempool.vue
@@ -16,7 +16,7 @@
           <td>{{ tx.data }}</td>
         </tr>
         <tr v-if="mempool && mempool.length === 0">
-          <td class="text-center" colspan="3">No txs in mempool.</td>
+          <td class="text-center" colspan="3">No unconfirmed TXs.</td>
         </tr>
         <infinite-loading force-use-infinite-wrapper="true" @infinite="infiniteHandler">
           <span slot="no-more"></span>

714015f0902b8eb4c2d096680db5e9f6af7f2e5a
Dan Anderson <anderson@familymedia.org>
2018-09-14 02:30:40 -0400

    Revert

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,9 +59,5 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'localize' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
-        'localizationRedirect' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
-        'localeSessionRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
-        'localeViewPath' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -11,28 +11,19 @@
 |
 */
 
-Route::group(
-[
-    'prefix' => LaravelLocalization::setLocale(),
-    'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
-],
-function()
-{
-    Route::get('/', 'HomeController@index')->name('home.index');
-    Route::get('/locale/{locale}', 'LocaleController@show')->name('locale.show');
-    Route::get('/assets', 'AssetsController@index')->name('assets.index');
-    Route::get('/asset/{asset}', 'AssetsController@show')->name('assets.show');
-    Route::get('/markets/{quote_asset?}', 'MarketsController@index')->name('markets.index');
-    Route::get('/market/{market}', 'MarketsController@show')->name('markets.show');
-    Route::get('/matches', 'OrderMatchesController@index')->name('matches.index');
-    Route::get('/orders', 'OrdersController@index')->name('orders.index');
-    Route::get('/blocks', 'BlocksController@index')->name('blocks.index');
-    Route::get('/block/{block}', 'BlocksController@show')->name('blocks.show');
-    Route::get('/mempool', 'MempoolController@index')->name('mempool.index');
-    Route::get('/disclaimer', 'PagesController@disclaimer')->name('pages.disclaimer');
-    Route::get('/privacy', 'PagesController@privacy')->name('pages.privacy');
-    Route::get('/terms', 'PagesController@terms')->name('pages.terms');
-    Auth::routes();
-});
-
+Auth::routes();
+Route::get('/', 'HomeController@index')->name('home.index');
+Route::get('/locale/{locale}', 'LocaleController@show')->name('locale.show');
+Route::get('/assets', 'AssetsController@index')->name('assets.index');
+Route::get('/asset/{asset}', 'AssetsController@show')->name('assets.show');
+Route::get('/markets/{quote_asset?}', 'MarketsController@index')->name('markets.index');
+Route::get('/market/{market}', 'MarketsController@show')->name('markets.show');
+Route::get('/matches', 'OrderMatchesController@index')->name('matches.index');
+Route::get('/orders', 'OrdersController@index')->name('orders.index');
+Route::get('/blocks', 'BlocksController@index')->name('blocks.index');
+Route::get('/block/{block}', 'BlocksController@show')->name('blocks.show');
+Route::get('/mempool', 'MempoolController@index')->name('mempool.index');
+Route::get('/disclaimer', 'PagesController@disclaimer')->name('pages.disclaimer');
+Route::get('/privacy', 'PagesController@privacy')->name('pages.privacy');
+Route::get('/terms', 'PagesController@terms')->name('pages.terms');
 Route::get('/{asset}', 'AssetsController@show');

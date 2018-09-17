8c1f132ca1244085b86ca501fe2baaa71bd66311
Dan Anderson <anderson@familymedia.org>
2018-09-17 17:49:26 -0400

    Update Search

--- app/Http/Controllers/Api/MarketSearchController.php
+++ app/Http/Controllers/Api/MarketSearchController.php
@@ -21,15 +21,18 @@ class MarketSearchController extends Controller
         // Validation
         $request->validate(['q' => 'required']);
 
+        // Trim Query
+        $query = trim($request->q);
+
         // Cache Slug
-        $cache_slug = 'api_search_' . str_slug(serialize($request->all()));
+        $cache_slug = 'api_search_' . str_slug($query);
 
         // Get Markets
-        return Cache::remember($cache_slug, 1440, function () use ($request) {
+        return Cache::remember($cache_slug, 1440, function () use ($query) {
             // Top 10
-            $markets = Market::where('xcp_core_base_asset', 'like', $request->q . '%')
-                ->orWhere('xcp_core_quote_asset', 'like', $request->q . '%')
-                ->orWhere('name', 'like', $request->q . '%')
+            $markets = Market::where('xcp_core_base_asset', 'like', $query . '%')
+                ->orWhere('xcp_core_quote_asset', 'like', $query . '%')
+                ->orWhere('name', 'like', $query . '%')
                 ->orderBy('volume', 'desc')
                 ->take(10)
                 ->get();

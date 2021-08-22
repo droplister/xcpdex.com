3413155a7343e8568f709e19520ed0cc4164347a
Dan Anderson <anderson@familymedia.org>
2021-08-22 01:40:26 -0400

    Update index.blade.php

--- resources/views/markets/index.blade.php
+++ resources/views/markets/index.blade.php
@@ -4,7 +4,14 @@
 @section('description', 'Explore the Counterparty DEX.')
 
 @section('content')
-    <h1 class="mb-3">DEX Markets</h1>
+    @if(! isset($quote_asset))
+        <h1>{{ __('Counterparty DEX') }}</h1>
+        <p class="lead">{{ __('The Decentralized Exchange on Bitcoin') }}</p>
+        <img src="{{ asset('images/how-it-works.png') }}" alt="XCP DEX" class="img-responsive my-2" width="100%" />
+    @else
+        <h1 class="mb-3">{{ $quote_asset }}</h1>
+        <p class="lead">Open orders for {{ $quote_asset }} on the Counterparty Dex.</p>
+    @endif
     @include('markets.partials.filter')
     <markets quote_asset="{{ $quote_asset }}"></markets>
 @endsection

43aeb62d9360663ad0607a4f4efb0cdf2631ab82
Dan Anderson <anderson@familymedia.org>
2021-08-22 17:53:03 -0400

    Update ViewServiceProvider.php

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Cache;
+use Droplister\XcpCore\App\Dispense;
+use Droplister\XcpCore\App\Dispenser;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,12 +17,20 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $price_data = Cache::remember('nav_prices', 30, function () {
+        $price_data = Cache::remember('usd_prices', 1440, function () {
             $cmc = new \CoinMarketCap\Api(config('xcpdex.coinmarketcap'));
 
-                $response1 = $cmc->cryptocurrency()->quotesLatest(['id' => 1, 'convert' => 'USD']);
-                $response2 = $cmc->cryptocurrency()->quotesLatest(['id' => 132, 'convert' => 'USD']);
-                $response3 = $cmc->cryptocurrency()->quotesLatest(['id' => 1405, 'convert' => 'USD']);
+            $response1 = $cmc->cryptocurrency()->quotesLatest(['id' => 1, 'convert' => 'USD']);
+            $response2 = $cmc->cryptocurrency()->quotesLatest(['id' => 132, 'convert' => 'USD']);
+            $response3 = $cmc->cryptocurrency()->quotesLatest(['id' => 1405, 'convert' => 'USD']);
+
+            $bitcorn_price = 0;
+            $last_dispense = Dispense::whereAsset('BITCORN')->latest('confirmed_at')->first();
+
+            if ($last_dispense) {
+                $dispenser = Dispenser::whereTxHash($last_dispense->dispenser_tx_hash)->first();
+                $bitcorn_price = fromSatoshi($dispenser->satoshirate / $dispenser->give_quantity) * $response1->data->{1}->quote->USD->price;
+            }
 
             return [
                 'BTC' => [
@@ -34,6 +44,9 @@ class ViewServiceProvider extends ServiceProvider
                 'PEPECASH' => [
                     'price' => number_format($response3->data->{1405}->quote->USD->price, 2),
                     'change' => number_format($response3->data->{1405}->quote->USD->percent_change_24h, 0),
+                ],
+                'BITCORN' => [
+                    'price' => number_format($bitcorn_price, 4),
                 ],
             ];
         });

178af53b6d3a53244327ccfbd3064bc32a54ad4d
Dan Anderson <anderson@familymedia.org>
2021-08-22 00:44:57 -0400

    Update AddressBalance.vue

--- resources/assets/js/components/AddressBalance.vue
+++ resources/assets/js/components/AddressBalance.vue
@@ -1,6 +1,8 @@
 <template>
 <span>
-  <small class="lead d-none d-sm-inline">{{ balance }} BTC ({{ transactions }} txs)</small>
+  <small class="lead d-none d-sm-inline">
+    {{ balance }} BTC <span style="font-size: 70%">({{ transactions }} txs)</span>
+  </small>
 </span>
 </template>
 
@@ -9,13 +11,13 @@ export default {
   props: ['address'],
   data () {
     return {
-      balance: 0.00000000,
+      balance: '0.00000000',
       transactions: 0,
     }
   },
   mounted: function() {
-    axios.get('https://blockstream.info/api/address/' + this.address).then(response => {
-      this.balance = response.data.chain_stats.funded_txo_sum - response.data.chain_stats.spent_txo_sum
+    axios.get('http://blockstream.info/api/address/' + this.address).then(response => {
+      this.balance = (response.data.chain_stats.funded_txo_sum - response.data.chain_stats.spent_txo_sum / 100000000).toFixed(8)
       this.transactions = response.data.chain_stats.tx_count
     })
   }

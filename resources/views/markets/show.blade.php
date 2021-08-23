f3c634850486365983c5f760109b8489c6685952
Dan Anderson <anderson@familymedia.org>
2021-08-22 20:18:06 -0400

    fdsfs

--- resources/views/markets/show.blade.php
+++ resources/views/markets/show.blade.php
@@ -15,7 +15,7 @@
     @if($last_match && $last_match->confirmed_at->diffInDays(Carbon\Carbon::now()) > 365)
         <div class="alert alert-warning">
             <i aria-hidden="true" class="fa fa-exclamation-circle mr-1"></i>
-             The last order in this market was created {{ $last_match->confirmed_at->toDateString() }}.
+            <strong>No Recent Activity:</strong> The last order was {{ $last_match->confirmed_at->toDateString() }}.
         </div>
     @endif
     <market-chart market="{{ $market->slug }}"

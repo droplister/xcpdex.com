a04e1cc5ffa5de48988f56235fbef19bb972c917
Dan Anderson <anderson@familymedia.org>
2021-08-23 08:43:43 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,14 +18,14 @@
     <meta name="description" content="@yield('description')">
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://xcpdex.com/js/app.js" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css?test') }}" rel="stylesheet">
+    <link href="https://xcpdex.com/css/app.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">

b6ce49c73b9175f0233d6bbc369769e9e94ed891
Dan Anderson <anderson@familymedia.org>
2021-08-23 23:38:19 -0400

    Update MarketsController.php

--- app/Http/Controllers/MarketsController.php
+++ app/Http/Controllers/MarketsController.php
@@ -33,7 +33,7 @@ class MarketsController extends Controller
 
         // Market Data
         $data = Cache::remember('market_index_' . $quote_asset, 1440, function () use ($quote_asset) {
-            $query = Market::where('xcp_core_quote_asset', $quote_asset);
+            $query = Market::where('xcp_core_quote_asset', $quote_asset)->where('open_orders_count', '>', 0);
 
             return [
                 'trading_pairs' => $query->count(),

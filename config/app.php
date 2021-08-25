acd948dbde8d69d154d457390df1451b509d4920
Dan Anderson <anderson@familymedia.org>
2021-08-25 04:23:51 -0400

    TG

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Telegram\Bot\Laravel\TelegramServiceProvider::class,
 
         /*
          * Application Service Providers...

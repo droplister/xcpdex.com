58533672b8c3b6f6d042e57e2c038f019248354e
Dan Anderson <anderson@familymedia.org>
2021-08-29 22:01:35 -0400

    Update OrderListener.php

--- app/Listeners/OrderListener.php
+++ app/Listeners/OrderListener.php
@@ -63,10 +63,10 @@ class OrderListener
 	        });
 
 
-        	$usd_value = $event->order->trading_total_normalized * str_replace(',', '', $price_data[$event->order->trading_pair_quote_asset]['price']);
+        	$usd_value = $event->order->trading_price_normalized * str_replace(',', '', $price_data[$event->order->trading_pair_quote_asset]['price']);
     		$usd_value = number_format($usd_value);
     		$amount_traded = str_replace('.00000000', '', $event->order->trading_quantity_normalized);
-	        $message = "*{$event->order->trading_type}* {$amount_traded} [{$event->order->trading_pair_base_asset}](https://xchain.io/asset/{$event->order->trading_pair_base_asset})\n   @ {$event->order->trading_price_normalized} {$event->order->trading_pair_quote_asset}\n--\nTotal: {$usd_value} USD  [order](https://xchain.io/tx/{$event->order->tx_index})";
+	        $message = "*{$event->order->trading_type}* {$amount_traded} [{$event->order->trading_pair_base_asset}](https://xchain.io/asset/{$event->order->trading_pair_base_asset})\n   @ {$event->order->trading_price_normalized} {$event->order->trading_pair_quote_asset}\n--\nPrice: {$usd_value} USD  [order](https://xchain.io/tx/{$event->order->tx_index})";
 
 	        SendTelegramMessage::dispatch($message, config('xcpdex.private_channel_id'));
         }

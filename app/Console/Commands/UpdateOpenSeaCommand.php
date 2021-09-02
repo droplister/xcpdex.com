37dd4a5765edcd05d529ac44cb63644b26eecb95
Dan Anderson <anderson@familymedia.org>
2021-09-02 03:04:41 -0400

    Update UpdateOpenSeaCommand.php

--- app/Console/Commands/UpdateOpenSeaCommand.php
+++ app/Console/Commands/UpdateOpenSeaCommand.php
@@ -73,8 +73,10 @@ class UpdateOpenSeaCommand extends Command
             $partial_test_2 = explode('.', str_replace('https://xchain.io/img/cards/', '', $event['asset']['image_original_url']))[0];
 
             // Do Name Match
-            if($asset = Asset::whereAssetName($partial_test_1)->orWhere('asset_longname', $partial_test_1)->exists() ||
-               $asset = Asset::whereAssetName($partial_test_2)->orWhere('asset_longname', $partial_test_2)->exists())
+            if($asset = Asset::where('asset_name', $partial_test_1)
+                ->orWhere('asset_name', $partial_test_2)
+                ->orWhere('asset_longname', $partial_test_1)
+                ->orWhere('asset_longname', $partial_test_2)->first())
             {
                 $asset_name = $asset->asset_name;
             }

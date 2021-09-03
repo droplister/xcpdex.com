468f696de99d82614fbbd1dc51fac798023699ac
Dan Anderson <anderson@familymedia.org>
2021-09-02 21:01:41 -0400

    sssss

--- database/migrations/2021_09_02_123456_add_trade_price_payment_token_to_open_seas_table.php
+++ database/migrations/2021_09_02_123456_add_trade_price_payment_token_to_open_seas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateOpenSeasColumns extends Migration
+class AddTradePricePaymentTokenToOpenSeasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,6 @@ class UpdateOpenSeasColumns extends Migration
     {
         Schema::table('open_seas', function (Blueprint $table) {
             $table->string('trade_price_payment_token')->nullable()->after('quantity');
-            $table->dropColumn('total_volume_payment_token');
         });
     }
 
@@ -28,7 +27,6 @@ class UpdateOpenSeasColumns extends Migration
     {
         Schema::table('open_seas', function (Blueprint $table) {
             $table->dropColumn('trade_price_payment_token')->nullable()->after('quantity');
-            $table->unsignedBigInteger('total_volume_payment_token')->after('total_volume_usd');
         });       
     }
 }

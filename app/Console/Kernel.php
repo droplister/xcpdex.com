70dd55f9b52ef5476d95c5b903faf85998f826f9
Dan Anderson <anderson@familymedia.org>
2021-09-13 01:56:11 -0400

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,6 +30,7 @@ class Kernel extends ConsoleKernel
             $schedule->command('update:index')->everyMinute();
             $schedule->command('update:mempool')->everyMinute();
             $schedule->command('update:opensea')->everyFiveMinutes();
+            $schedule->command('update:scarcecity')->everyFiveMinutes();
             $schedule->command('update:markets')->daily();
         }
     }
